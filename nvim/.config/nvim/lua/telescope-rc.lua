6bd9dc0d17a4d97d19796560e2a50e42ce2e6793
Optyx <88157475+0ptyx@users.noreply.github.com>
2023-11-11 12:49:13 -0500

    Add telescope keybindings

--- nvim/.config/nvim/lua/telescope-rc.lua
+++ nvim/.config/nvim/lua/telescope-rc.lua
@@ -64,6 +64,14 @@ vim.keymap.set("n", "<leader>ps", function()
 --         require('telescope.builtin').grep_string({ search = vim.fn.input("Grep for > ")}) 
 --     end, {noremap = true})
 
+vim.keymap.set("n", "<leader>pp", function()
+  require('telescope.builtin').builtin()
+end, {noremap = true})
+
+vim.keymap.set("n", "<leader>pb", function()
+  require('telescope.builtin').buffers()
+end, {noremap = true})
+
 
 -- Grep for the conitinuous word under your cursor
 vim.keymap.set("n", "<leader>pw", function()
